55c71d00d1849b1457b0772d494fb577abc3880f
xodn__love@naver.com <xodn__love@naver.com>
2024-07-23 14:32:51 +0900

    프로젝트 초기 환경 설정

---
+++ src/test/java/com/lgdx/testapi/TestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgdx.testapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
